a79aedabe310d09e3980bb923e91e57cc35cb436
kkkkkksssssaaaa <kofc312@naver.com>
2022-10-19 00:36:43 +0900

    test(Amount): add display name

--- src/test/java/truck/menu/AmountTest.java
+++ src/test/java/truck/menu/AmountTest.java
@@ -1,5 +1,11 @@
 package truck.menu;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayName("Amount")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class AmountTest {
 
 }
