28f55ff616368d9477f4cb9adb544cef76f3a211
kkkkkksssssaaaa <kofc312@naver.com>
2022-10-19 00:19:43 +0900

    feat: add boong-eo-bbang's amount object

---
+++ src/main/java/truck/menu/Amount.java
@@ -0,0 +1,18 @@
+package truck.menu;
+
+public class Amount {
+
+    private final int amount;
+
+    private Amount(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.amount = amount;
+    }
+
+    public static Amount of(int amount) {
+        return new Amount(amount);
+    }
+}
