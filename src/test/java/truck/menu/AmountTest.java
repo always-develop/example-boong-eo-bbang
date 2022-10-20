93aca335faf54a74515785f4eb8eb6354c43b545
kkkkkksssssaaaa <kofc312@naver.com>
2022-10-20 23:52:22 +0900

    test(Amount): add initialize failed test

--- src/test/java/truck/menu/AmountTest.java
+++ src/test/java/truck/menu/AmountTest.java
@@ -3,9 +3,22 @@ package truck.menu;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Amount")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class AmountTest {
 
+    @Nested
+    @DisplayName("Initialize")
+    class InitializeTest {
+
+        @Test
+        void 금액이_0보다_적으면_IllegalArgumentException을_던진다() {
+            assertThrows(IllegalArgumentException.class, () -> Amount.of(-1));
+        }
+    }
 }
