a2e3aaccbcc08e351c16ad3ff27c8bb2dad7ff85
kkkkkksssssaaaa <kofc312@naver.com>
2022-10-20 23:54:53 +0900

    test(Amount): add initialize success test

--- src/test/java/truck/menu/AmountTest.java
+++ src/test/java/truck/menu/AmountTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Amount")
@@ -19,6 +20,16 @@ class AmountTest {
         @Test
         void 금액이_0보다_적으면_IllegalArgumentException을_던진다() {
             assertThrows(IllegalArgumentException.class, () -> Amount.of(-1));
+        }
+
+        @Test
+        void 금액은_0일_수_있다() {
+            assertDoesNotThrow(() -> Amount.of(0));
+        }
+
+        @Test
+        void 금액은_0보다_클_수_있다() {
+            assertDoesNotThrow(() -> Amount.of(1));
         }
     }
 }
