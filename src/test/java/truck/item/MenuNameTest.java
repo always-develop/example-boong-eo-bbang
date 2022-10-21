05aeed822049bcdfbf86fcd5771eccb39c1bbdd8
kkkkkksssssaaaa <kofc312@naver.com>
2022-10-21 23:43:51 +0900

    test: add empty class of MenuName

---
+++ src/test/java/truck/item/MenuNameTest.java
@@ -0,0 +1,13 @@
+package truck.item;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("MenuName")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class MenuNameTest {
+
+}
