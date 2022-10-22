fd3379743fddbe080e0f272cac04593acef405bf
kkkkkksssssaaaa <kofc312@naver.com>
2022-10-22 23:27:29 +0900

    refactor(CheckInteger): change literal number to constant

--- src/main/java/util/CheckInteger.java
+++ src/main/java/util/CheckInteger.java
@@ -2,8 +2,11 @@ package util;
 
 public class CheckInteger {
 
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+
     public static void moreThenOne(int integer) {
-        if (1 > integer) {
+        if (ONE > integer) {
             throw new IllegalArgumentException();
         }
     }
