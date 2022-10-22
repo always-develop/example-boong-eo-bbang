6b5f6b723b150d412e41d9c23ce9bd28ee16041e
kkkkkksssssaaaa <kofc312@naver.com>
2022-10-23 01:18:20 +0900

    test(CheckInteger): add moreThenZero test

--- src/test/java/util/CheckIntegerTest.java
+++ src/test/java/util/CheckIntegerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("CheckInteger")
@@ -30,6 +32,27 @@ class CheckIntegerTest {
         void 인자가_1보다_크면_예외를_던지지_않는다() {
             assertDoesNotThrow(() -> CheckInteger.moreThenOne(2));
             assertDoesNotThrow(() -> CheckInteger.moreThenOne(Integer.MAX_VALUE));
+        }
+    }
+
+    @Nested
+    @DisplayName("MoreThenZero")
+    class MoreThenZeroTest {
+
+        @Test
+        void 인자가_0보다_작으면_IllegalArgumentException을_던진다() {
+            assertThrows(IllegalArgumentException.class, () -> CheckInteger.moreThenZero(-1));
+        }
+
+        @Test
+        void 인자가_0이면_예외를_던지지_않는다() {
+            assertDoesNotThrow(() -> CheckInteger.moreThenZero(0));
+        }
+
+        @Test
+        void 인자가_0보다_크면_예외를_던지지_않는다() {
+            assertDoesNotThrow(() -> CheckInteger.moreThenZero(1));
+            assertDoesNotThrow(() -> CheckInteger.moreThenZero(Integer.MAX_VALUE));
         }
     }
 }
