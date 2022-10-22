40c493644a3a06a6aba316f67c793b3c1c83bf8f
kkkkkksssssaaaa <kofc312@naver.com>
2022-10-23 01:15:53 +0900

    refactor(CheckIntegerTest): refactor divide area from method

--- src/test/java/util/CheckIntegerTest.java
+++ src/test/java/util/CheckIntegerTest.java
@@ -3,6 +3,7 @@ package util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,19 +12,24 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CheckIntegerTest {
 
-    @Test
-    void 인자가_1보다_작으면_IllegalArgumentException을_던진다() {
-        assertThrows(IllegalArgumentException.class, () -> CheckInteger.moreThenOne(0));
-    }
+    @Nested
+    @DisplayName("MoreThenOne")
+    class MoreThenOneTest {
 
-    @Test
-    void 인자가_1이면_예외를_던지지_않는다() {
-        assertDoesNotThrow(() -> CheckInteger.moreThenOne(1));
-    }
+        @Test
+        void 인자가_1보다_작으면_IllegalArgumentException을_던진다() {
+            assertThrows(IllegalArgumentException.class, () -> CheckInteger.moreThenOne(0));
+        }
 
-    @Test
-    void 인자가_1보다_크면_예외를_던지지_않는다() {
-        assertDoesNotThrow(() -> CheckInteger.moreThenOne(2));
-        assertDoesNotThrow(() -> CheckInteger.moreThenOne(Integer.MAX_VALUE));
+        @Test
+        void 인자가_1이면_예외를_던지지_않는다() {
+            assertDoesNotThrow(() -> CheckInteger.moreThenOne(1));
+        }
+
+        @Test
+        void 인자가_1보다_크면_예외를_던지지_않는다() {
+            assertDoesNotThrow(() -> CheckInteger.moreThenOne(2));
+            assertDoesNotThrow(() -> CheckInteger.moreThenOne(Integer.MAX_VALUE));
+        }
     }
 }
