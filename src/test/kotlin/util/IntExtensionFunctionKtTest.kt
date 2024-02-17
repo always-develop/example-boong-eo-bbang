8f4a40afe16ea1b4b70fc4baee5dac26298e75de
kkkkkksssssaaaa <kofc312@naver.com>
2024-02-17 23:22:28 +0900

    test: add Int.moreThenZero test

--- src/test/kotlin/util/IntExtensionFunctionKtTest.kt
+++ src/test/kotlin/util/IntExtensionFunctionKtTest.kt
@@ -7,7 +7,7 @@ class IntExtensionFunctionKtTest {
 
     @Nested
     @DisplayName("Int.moreThenOne")
-    inner class moreThenOneTest {
+    inner class MoreThenOneTest {
 
         @Test
         fun `숫자가 1보다 크면 예외를 던지지 않는다`() {
@@ -33,6 +33,38 @@ class IntExtensionFunctionKtTest {
 
             assertThrows<IllegalArgumentException> {
                 zero.moreThenOne()
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("Int.moreThenZero")
+    inner class MoreThenZeroTest {
+
+        @Test
+        fun `숫자가 0보다 크면 예외를 던지지 않는다`() {
+            val one = 1
+
+            assertDoesNotThrow {
+                one.moreThenZero()
+            }
+        }
+
+        @Test
+        fun `숫자가 0이면 예외를 던지지 않는다`() {
+            val zero = 0
+
+            assertDoesNotThrow {
+                zero.moreThenZero()
+            }
+        }
+
+        @Test
+        fun `숫자가 0보다 작으면 IllegalArgumentException 을 던진다`() {
+            val minus = -1
+
+            assertThrows<IllegalArgumentException> {
+                minus.moreThenZero()
             }
         }
     }
