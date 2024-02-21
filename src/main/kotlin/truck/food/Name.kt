3ad54cc671b52eb44c59667816c755eada15410d
kkkkkksssssaaaa <kofc312@naver.com>
2024-02-21 23:05:02 +0900

    feat: add kotlin object Name

---
+++ src/main/kotlin/truck/food/Name.kt
@@ -0,0 +1,9 @@
+package truck.food
+
+import org.jetbrains.annotations.NotNull
+
+// TODO: add custom annotation, check empty string and length
+data class Name(
+    @field:NotNull
+    val name: String
+)
