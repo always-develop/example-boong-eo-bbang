dd245ade1fc8f934ae746109a2ebf2d003dce602
kkkkkksssssaaaa <kofc312@naver.com>
2024-02-24 23:51:22 +0900

    feat: add validation to kotlin object Name

--- src/main/kotlin/truck/food/Name.kt
+++ src/main/kotlin/truck/food/Name.kt
@@ -1,9 +1,17 @@
 package truck.food
 
-import org.jetbrains.annotations.NotNull
+data class Name(private val name: String) {
+    companion object {
+        private const val MAX_LENGTH: Int = 12
+        private const val MIN_LENGTH: Int = 1
+    }
 
-// TODO: add custom annotation, check empty string and length
-data class Name(
-    @field:NotNull
-    val name: String
-)
+    init {
+        if (MIN_LENGTH >= name.length) {
+            throw IllegalArgumentException("Name length is must be more then $MIN_LENGTH")
+        }
+        if (MAX_LENGTH <= name.length) {
+            throw IllegalArgumentException("Name length is must be less then $MAX_LENGTH")
+        }
+    }
+}
